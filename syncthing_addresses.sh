822c67c008ca9a748b0501875386e64cdca87f8c
s-myk <miyake@dtc.ch>
2026-01-21 14:26:58 +0900

    Fix `syncthing_addresses.sh`

--- syncthing_addresses.sh
+++ syncthing_addresses.sh
@@ -7,7 +7,7 @@ set -eux
 
 script_dir_path=$(dirname $(readlink -f $0))
 
-tailscale status | awk '/100./ {printf("dynamic, tcp4://%s, tcp4://%s, dynamic\n", $2, $1)};' | sort > $script_dir_path/syncthing_adresses.txt
+tailscale.exe status | awk '/100./ {printf("dynamic, tcp4://%s, tcp4://%s, dynamic\n", $2, $1)};' | sort > $script_dir_path/syncthing_addresses.txt
 
 # crontab -e
 # */10 *    *   *   * ~/Sync/syncthing_adresses.sh
